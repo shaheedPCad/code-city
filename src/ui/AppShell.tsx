678079796e21f39a2da4a0bba23036c2285520f2
Shaheed Ahmed <shaheedpcad@gmail.com>
2026-01-17 23:55:06 -0500

    stage changes

---
+++ src/ui/AppShell.tsx
@@ -0,0 +1,13 @@
+import { ControlPanel } from './ControlPanel'
+import { CityViewport } from './CityViewport'
+import { TelemetryPanel } from './TelemetryPanel'
+
+export function AppShell() {
+  return (
+    <div className="h-screen w-screen grid grid-cols-[320px_1fr_320px]">
+      <ControlPanel />
+      <CityViewport />
+      <TelemetryPanel />
+    </div>
+  )
+}
